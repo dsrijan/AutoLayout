4ddbc18063e4ba4a6d55985cc1b9dab6a4e8cea8
dsrijan <srijan12345@gmail.com>
2018-03-30 12:50:32 +0530

    Auto Layout Project For Swift 4

--- AutoLayoutLearning/AppDelegate.swift
+++ AutoLayoutLearning/AppDelegate.swift
@@ -2,11 +2,12 @@
 //  AppDelegate.swift
 //  AutoLayoutLearning
 //
-//  Created by DRISHTI-IT on 27/03/18.
-//  Copyright © 2018 DRISHTI-IT. All rights reserved.
+//  Created by Srijan on 27/03/18.
+//  Copyright © 2018 Srijan. All rights reserved.
 //
 
 import UIKit
+import UserNotifications
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -18,7 +19,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         // Override point for customization after application launch.
         return true
     }
-
+    
     func applicationWillResignActive(_ application: UIApplication) {
         // Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.
         // Use this method to pause ongoing tasks, disable timers, and invalidate graphics rendering callbacks. Games should use this method to pause the game.
--- AutoLayoutLearning/ViewController.swift
+++ AutoLayoutLearning/ViewController.swift
@@ -2,17 +2,119 @@
 //  ViewController.swift
 //  AutoLayoutLearning
 //
-//  Created by DRISHTI-IT on 27/03/18.
-//  Copyright © 2018 DRISHTI-IT. All rights reserved.
+//  Created by Srijan on 27/03/18.
+//  Copyright © 2018 Srijan. All rights reserved.
 //
 
 import UIKit
 
 class ViewController: UIViewController {
+    
+    let imageView : UIImageView = {
+       let tempImageView = UIImageView()
+       tempImageView.backgroundColor = UIColor.yellow
+       tempImageView.contentMode = .scaleAspectFit
+       tempImageView.clipsToBounds = true
+       
+        // we need to set translatesAutoresizingMaskIntoConstraints propoerty as false to provide constraints programmetically
+       tempImageView.translatesAutoresizingMaskIntoConstraints = false
+       return tempImageView
+    }()
+    
+    let topHalfView : UIView = {
+       let view = UIView(frame: .zero)
+       view.backgroundColor = UIColor.purple
+       view.isUserInteractionEnabled = true
+       
+        // we need to set translatesAutoresizingMaskIntoConstraints propoerty as false to provide constraints programmetically
+       view.translatesAutoresizingMaskIntoConstraints = false
+      
+        return view
+    }()
+    
+    let textView : UITextView = {
+       let tempTextView = UITextView()
+       tempTextView.backgroundColor = UIColor.lightGray
+     
+        // we need to set translatesAutoresizingMaskIntoConstraints propoerty as false to provide constraints programmetically
+       tempTextView.translatesAutoresizingMaskIntoConstraints = false
+       
+        // Paragraph style enables you to change alingment
+        let titleParagraphStyle = NSMutableParagraphStyle()
+        titleParagraphStyle.alignment = .center
+        
+        // Mutliple line in a textview with different style of strings
+        
+        var tempAttributedString = NSMutableAttributedString(string: "My Name Is Srijan", attributes: [NSAttributedStringKey.font : UIFont.boldSystemFont(ofSize: 18.0), NSAttributedStringKey.foregroundColor: UIColor.darkText , NSAttributedStringKey.paragraphStyle : titleParagraphStyle ])
+       
+        tempAttributedString.append(NSAttributedString(string: "\n\n\n Srijan Kumar", attributes: [NSAttributedStringKey.font : UIFont.systemFont(ofSize: 14.0), NSAttributedStringKey.paragraphStyle : titleParagraphStyle]))
+        
+        tempTextView.textAlignment = .center
+       tempTextView.attributedText = tempAttributedString
+       return tempTextView
+    }()
+    
+    
+    let bottomStackView: UIStackView = {
+        let redView = UIView()
+        redView.translatesAutoresizingMaskIntoConstraints = false
+        redView.backgroundColor = UIColor.red
+        
+        let blueView = UIView()
+        blueView.translatesAutoresizingMaskIntoConstraints = false
+        blueView.backgroundColor = UIColor.blue
+        
+        let greenView = UIView()
+        greenView.translatesAutoresizingMaskIntoConstraints = false
+        greenView.backgroundColor = UIColor.green
+ 
+        let stackView = UIStackView(arrangedSubviews: [redView,blueView,greenView])
+        
+        if UIDevice.current.userInterfaceIdiom == UIUserInterfaceIdiom.phone {
+            stackView.axis = .horizontal
+        }else{
+            stackView.axis = .vertical
+        }
+        
+        stackView.distribution = .fillEqually
+        stackView.translatesAutoresizingMaskIntoConstraints = false
+        return stackView
+    }()
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        setUpImageView()
         // Do any additional setup after loading the view, typically from a nib.
+    }
+    
+    private func setUpImageView() {
+        
+        view.addSubview(topHalfView)
+        topHalfView.leadingAnchor.constraint(equalTo: self.view.leadingAnchor).isActive = true
+        topHalfView.topAnchor.constraint(equalTo: self.view.topAnchor).isActive = true
+        topHalfView.trailingAnchor.constraint(equalTo: self.view.trailingAnchor).isActive = true
+        topHalfView.heightAnchor.constraint(equalTo: self.view.heightAnchor, multiplier: 0.5).isActive = true
+        
+        topHalfView.addSubview(self.imageView)
+        imageView.centerYAnchor.constraint(equalTo: topHalfView.centerYAnchor).isActive = true
+        imageView.centerXAnchor.constraint(equalTo: topHalfView.centerXAnchor).isActive = true
+        imageView.heightAnchor.constraint(equalTo: topHalfView.heightAnchor, multiplier: 0.5).isActive = true
+        imageView.widthAnchor.constraint(equalTo: topHalfView.widthAnchor, multiplier: 0.60).isActive = true
+        
+       
+        view.addSubview(textView)
+        textView.leadingAnchor.constraint(equalTo: self.view.safeAreaLayoutGuide.leadingAnchor).isActive = true
+        textView.trailingAnchor.constraint(equalTo: self.view.safeAreaLayoutGuide.trailingAnchor).isActive = true
+        textView.bottomAnchor.constraint(equalTo: self.view.safeAreaLayoutGuide.bottomAnchor).isActive = true
+        textView.topAnchor.constraint(equalTo: topHalfView.bottomAnchor).isActive = true
+        
+        view.addSubview(bottomStackView)
+        bottomStackView.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor).isActive = true
+        bottomStackView.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor).isActive = true
+        bottomStackView.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor).isActive = true
+        bottomStackView.heightAnchor.constraint(equalToConstant: 50).isActive = true
+        
+        
     }
 
     override func didReceiveMemoryWarning() {
